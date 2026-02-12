21924cf43e5075dcf487b703c63a61019ebb7082
Karthik Ambu <karthik@bullitt.com>
2026-02-12 15:23:14 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,5 @@ fn main() {
 
     let mut command = String::new();
     io::stdin().read_line(&mut command).unwrap();
-    print!("{}: command not found", command.trim());
+    println!("{}: command not found", command.trim());
 }
