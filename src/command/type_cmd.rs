c76a1f8b76db5ecb63fd5b3a93b9578d10e13214
Karthik Ambu <karthik@bullitt.com>
2026-02-12 18:12:44 +0000

    Add type to look in PATH

--- src/command/type_cmd.rs
+++ src/command/type_cmd.rs
@@ -1,4 +1,6 @@
 use std::fmt;
+use std::os::unix::fs::PermissionsExt;
+use std::path::Path;
 
 use super::{parse, Command};
 
@@ -15,10 +17,25 @@ impl Command for TypeCmd {
         let command = parse(&self.args);
         if command.is_builtin() {
             println!("{} is a shell builtin", command);
+        } else if let Some(path) = find_in_path(&self.args) {
+            println!("{} is {}", self.args, path);
         } else {
             println!("{}: not found", command);
         }
     }
+}
+
+fn find_in_path(arg: &str) -> Option<String> {
+    let path_var = std::env::var("PATH").ok()?;
+    for dir in path_var.split(':') {
+        let full_path = Path::new(dir).join(arg);
+        if let Ok(metadata) = full_path.metadata() {
+            if metadata.is_file() && (metadata.permissions().mode() & 0o111 != 0) {
+                return Some(full_path.to_string_lossy().into_owned());
+            }
+        }
+    }
+    None
 }
 
 impl fmt::Display for TypeCmd {
